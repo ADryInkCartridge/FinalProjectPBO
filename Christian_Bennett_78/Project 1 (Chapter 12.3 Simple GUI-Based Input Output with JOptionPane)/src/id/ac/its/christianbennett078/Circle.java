2c94bc307b6d629110a0fd26894d79a3eae2faf3
ADryInkCartridge <bayuaaas@gmail.com>
2020-12-12 20:55:53 +0700

    BENNETT NUMPANG

---
+++ Christian_Bennett_78/Project 1 (Chapter 12.3 Simple GUI-Based Input Output with JOptionPane)/src/id/ac/its/christianbennett078/Circle.java
@@ -0,0 +1,16 @@
+package id.ac.its.christianbennett078;
+
+import javax.swing.JOptionPane;
+import java.lang.Math;
+
+public class Circle extends Shapes {
+
+    public void Calculate() {
+        String radius = JOptionPane.showInputDialog("Enter circle radius");
+        int numradius = Integer.parseInt(radius);
+        double area = Math.PI * Math.pow(numradius, 2);
+        double perimeter = Math.PI * 2 * numradius;
+        JOptionPane.showMessageDialog(null, "The area is " + area + "\nThe perimeter is " + perimeter,
+                "Area and perimeter of a circle", JOptionPane.PLAIN_MESSAGE);
+    }
+}
