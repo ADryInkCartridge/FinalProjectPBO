5a4025bdeced451a016f46728d1da3cebb4fe65e
Christoffer Ivano <christoffer.ivano@gmail.com>
2021-01-11 18:59:17 +0700

    add hscore, fix mainmenu

---
+++ Final Project/SnakeGame/src/id/ac/its/bayubennettivan/snakegame/HighScoreMenu.java
@@ -0,0 +1,55 @@
+package id.ac.its.bayubennettivan.snakegame;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.util.List;
+import id.ac.its.bayubennettivan.snakegame.Snake;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class HighScoreMenu extends Screen
+{
+    private Image backgroundHighScore, backMenu;
+    private List<User> playerScore;
+
+    public HighScoreMenu(JFrame referred, Image backgroundHighScore, Image backMenu) {
+        super(referred);
+        super.setImg(this.getImage("src/assets/background.png"));
+        super.setImg(this.getImage("src/assets/Back.png"));
+        this.playerScore = User.load("score.txt");
+    }
+
+	private Image getImage(String filename) {
+        try {
+            return ImageIO.read(new File(filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(backMenu, 250, 450, null);
+
+    }
+
+    @Override
+    public void stateChange(int state) {
+        switch(state) {
+            case 0:
+                referred.setContentPane(new MainMenu(referred));
+                referred.validate();
+                referred.getContentPane().requestFocusInWindow();
+                break;
+            }
+    }
+    
+}
--- Final Project/SnakeGame/src/id/ac/its/bayubennettivan/snakegame/MainMenu.java
+++ Final Project/SnakeGame/src/id/ac/its/bayubennettivan/snakegame/MainMenu.java
@@ -3,10 +3,14 @@ package id.ac.its.bayubennettivan.snakegame;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.MouseListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import id.ac.its.bayubennettivan.snakegame.Snake;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,12 +18,29 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class MainMenu extends Screen {
-    private Image backgroundMenu, newGameBtn;
+    private Image backgroundMenu, newGameBtn, hScoreMenu;
 
     public MainMenu(JFrame referred, Image backgroundMenu, Image newGameBtn) {
         super(referred);
         super.setImg(this.getImage("src/assets/background.png"));
         super.setImg(this.getImage("src/assets/NewGame.png"));
+        super.setImg(this.getImage("src/assets/HighScore.png"));
+
+        addMouseListener((MouseListener) new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				//untuk newGameBtn
+				if(e.getPoint().x >=250 && e.getPoint().x <=550 && e.getPoint().y >= 325 && e.getPoint().y <= 405){
+					stateChange(1);
+				};
+				
+				//untuk highScoreBtn
+				if(e.getPoint().x >=250 && e.getPoint().x <=550 && e.getPoint().y >= 425 && e.getPoint().y <= 505){
+					stateChange(2);
+				};
+			}
+		});
     }
 
     private Image getImage(String filename) {
@@ -35,14 +56,22 @@ public class MainMenu extends Screen {
     public void render(Graphics g) {
         g.drawImage(backgroundMenu, 0, 0, null);
         g.drawImage(newGameBtn, 250, 325, null);
+        g.drawImage(hScoreMenu, 250, 425, null);
     }
 
     @Override
     public void stateChange(int state) {
-        referred.setContentPane(new Gameplay(referred));
-        referred.validate();
-        referred.getContentPane().requestFocusInWindow();
-        // break;
+        switch(state) {
+            case 1:
+                referred.setContentPane(new Gameplay(referred));
+                referred.validate();
+                referred.getContentPane().requestFocusInWindow();
+                break;
+            case 2:
+                referred.setContentPane(new HighScoreMenu(referred, backgroundMenu, backgroundMenu));
+                referred.validate();
+                referred.getContentPane().requestFocusInWindow();
+            }
     }
 
 }
